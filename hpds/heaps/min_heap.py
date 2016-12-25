1b35535d0e5e17582a60974cfc1d1351596f031f
David Skoda <dskoda@bloomberg.net>
2016-12-25 15:02:45 -0500

    add heap and basic children/parent methods

--- hpds/heaps/min_heap.py
+++ hpds/heaps/min_heap.py
@@ -1,5 +1,19 @@
+from math import floor
+
 
 class MinHeap(object):
 
     def __init__(self):
-        pass
+        self._values = [None]
+
+
+    def _left_child(self, i):
+        return self._values[2 * i]
+
+    def _right_child(self, i):
+        return self._values[2 * i + 1]
+
+    def _parent(self, i):
+        i = float(i)
+        index = int(floor(i / 2))
+        return self._values[index]
--- tests/heaps/test_min_heap.py
+++ tests/heaps/test_min_heap.py
@@ -12,5 +12,40 @@ class TestMinHeap(unittest.TestCase):
 
     def setUp(self):
         self.heap = MinHeap()
+        self.heap._values = [None] + \
+            [i for i in range(1, 8)]
 
-    
+    def test_left_child(self):
+        self._test_equal_function(
+            MinHeap._left_child, 1, 2)
+        self._test_equal_function(
+            MinHeap._left_child, 2, 4)
+        self._test_equal_function(
+            MinHeap._left_child, 3, 6)
+
+    def test_right_child(self):
+        self._test_equal_function(
+            MinHeap._right_child, 1, 3)
+        self._test_equal_function(
+            MinHeap._right_child, 2, 5)
+        self._test_equal_function(
+            MinHeap._right_child, 3, 7)
+
+    def test_parent(self):
+        self._test_equal_function(
+            MinHeap._parent, 7, 3)
+        self._test_equal_function(
+            MinHeap._parent, 6, 3)
+        self._test_equal_function(
+            MinHeap._parent, 5, 2)
+        self._test_equal_function(
+            MinHeap._parent, 4, 2)
+        self._test_equal_function(
+            MinHeap._parent, 3, 1)
+        self._test_equal_function(
+            MinHeap._parent, 2, 1)
+        self._test_equal_function(
+            MinHeap._parent, 1, None)
+
+    def _test_equal_function(self, func, x, y):
+        self.assertEqual(func(self.heap, x), y)
